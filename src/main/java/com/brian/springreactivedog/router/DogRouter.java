5f52e4790f54ced4501a8eb7f10cb7517458613d
BrianSammit <briansammit.cruz@gmail.com>
2023-03-22 12:34:07 -0500

    Add get all dogs router

--- src/main/java/com/brian/springreactivedog/router/DogRouter.java
+++ src/main/java/com/brian/springreactivedog/router/DogRouter.java
@@ -1,16 +1,17 @@
 package com.brian.springreactivedog.router;
 
 import com.brian.springreactivedog.domain.DTO.DogDTO;
+import com.brian.springreactivedog.usecases.GetAllDogsUseCase;
 import com.brian.springreactivedog.usecases.SaveDogUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -25,5 +26,15 @@ public class DogRouter {
                                         .bodyValue(result))
 
                                 .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_ACCEPTABLE).build())));
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> getAllDogs(GetAllDogsUseCase getAllDogsUseCase) {
+        return route(GET("/dogs"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getAllDogsUseCase.get(), DogDTO.class))
+                        .onErrorResume(throwable -> ServerResponse.noContent().build()));
     }
 }
