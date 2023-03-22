9b9d1bb491aa293b49b074f3581dc31aeb146b5a
BrianSammit <briansammit.cruz@gmail.com>
2023-03-22 11:55:30 -0500

    Add dog save router

---
+++ src/main/java/com/brian/springreactivedog/router/DogRouter.java
@@ -0,0 +1,27 @@
+package com.brian.springreactivedog.router;
+
+import com.brian.springreactivedog.domain.DTO.DogDTO;
+import com.brian.springreactivedog.usecases.SaveDogUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+public class DogRouter {
+    @Bean
+    public RouterFunction<ServerResponse> saveDog(SaveDogUseCase saveDogUseCase){
+        return route(POST("/dogs").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(DogDTO.class)
+                        .flatMap(dogDTO -> saveDogUseCase.save(dogDTO)
+                                .flatMap(result -> ServerResponse.status(201)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_ACCEPTABLE).build())));
+    }
+}
