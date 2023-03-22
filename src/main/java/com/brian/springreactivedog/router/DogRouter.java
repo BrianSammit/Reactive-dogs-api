45571ea16cc1591c03b75dd88737cb0c63a67273
BrianSammit <briansammit.cruz@gmail.com>
2023-03-22 12:14:04 -0500

    Fix connection problems

--- src/main/java/com/brian/springreactivedog/router/DogRouter.java
+++ src/main/java/com/brian/springreactivedog/router/DogRouter.java
@@ -3,6 +3,7 @@ package com.brian.springreactivedog.router;
 import com.brian.springreactivedog.domain.DTO.DogDTO;
 import com.brian.springreactivedog.usecases.SaveDogUseCase;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -12,6 +13,7 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
+@Configuration
 public class DogRouter {
     @Bean
     public RouterFunction<ServerResponse> saveDog(SaveDogUseCase saveDogUseCase){
